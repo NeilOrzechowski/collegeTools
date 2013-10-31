a90d11d47d1c3f5343d2f7a1ba3e555a6aeba964
Neil Orzechowski <nvorzech@gmail.com>
2013-10-31 18:24:33 -0400

    project update

---
+++ src/backend/model/Schedule.java
@@ -0,0 +1,18 @@
+package backend.model;
+
+import java.sql.Time;
+
+public class Schedule {
+
+	
+	private String[] classes;
+	
+	private Time[] startTime;
+	
+	private Time[] endTime;
+	
+	private int number;
+	
+	
+	
+}
---
+++ src/backend/model/User.java
@@ -0,0 +1,12 @@
+package backend.model;
+
+public class User {
+	
+	private String name;//must be unique
+	
+	private Long id;
+	
+	private Schedule[] schedules;
+	
+
+}
